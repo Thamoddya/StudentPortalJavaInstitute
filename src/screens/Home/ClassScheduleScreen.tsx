81624d0ae5122de2a9db82b485458df7bc14fc62
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2024-10-01 02:52:07 +0530

    Stage 1 Complete

--- src/screens/Home/ClassScheduleScreen.tsx
+++ src/screens/Home/ClassScheduleScreen.tsx
@@ -1,7 +1,76 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import LectureCard from "../../components/cards/LectureCard";
 import { ScreenStyles, TextStyles } from "../../styles/AppStyles";
 import Colors from "../../styles/Colors";
+
+let dummyLectures = [
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 1",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: true,
+    url: "https://www.google.com",
+    time: "10:00 AM - 12:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 2",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "12:00 PM - 2:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 3",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "2:00 PM - 4:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 4",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "4:00 PM - 6:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 5",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "6:00 PM - 8:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 6",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "8:00 PM - 10:00 PM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 7",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "10:00 PM - 12:00 AM",
+  },
+  {
+    title: "AOOP-III - Advanced Object-Oriented Programming Day 8",
+    venue: "Kandy - Online",
+    lecturer: "Dr Tharaka Sankalpa",
+    date: "10-October-2024",
+    isToday: false,
+    time: "12:00 AM - 2:00 AM",
+  },
+];
 
 const ClassScheduleScreen = () => {
   // MARK: - Component
@@ -18,6 +87,20 @@ const ClassScheduleScreen = () => {
   return (
     <SafeAreaView style={[ScreenStyles.container]}>
       <HeaderView />
+      <ScrollView style={[ScreenStyles.subContainer]}>
+        {dummyLectures.map((item, index) => (
+          <LectureCard
+            key={index}
+            title={item.title}
+            venue={item.venue}
+            lecturer={item.lecturer}
+            date={item.date}
+            isToday={item.isToday}
+            url={item.url}
+            time={item.time}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
